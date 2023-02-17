25ee97da692a50fec93bae5d806552fa65eb0ee7
Goik <sbabinski@outlook.com>
2023-02-17 13:32:25 -0300

    Fix cart test
    Change user.click to fireEvent for now.

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -1,5 +1,11 @@
 import { vi } from "vitest";
-import { render, screen, act } from "@testing-library/react";
+import {
+  render,
+  screen,
+  act,
+  waitFor,
+  fireEvent,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 import React from "react";
@@ -10,18 +16,11 @@ describe("Tries to buy an item", () => {
   beforeEach(() => {
     render(<App />);
   });
-  it("Add an item to cart", async () => {
+  it("Changes the item amount in the cart", async () => {
     await user.click(screen.getAllByTitle(/Go to our products page/)[0]);
     await user.click(screen.getByTitle(/Add Anger/));
-    expect(screen.getByTitle(/Number of items on cart/).textContent).toBe("1");
-  });
-  it("Changes the item amount in the cart", () => {
-    act( async () => {
-       await user.click(screen.getAllByTitle(/Go to our products page/)[0]);
-       await user.click(screen.getByTitle(/Add Anger/));
-       await user.click(screen.getByTitle(/Shopping cart/));
-       await user.click(screen.getByTitle(/Add one/));
-      expect(screen.getByTitle(/Quantity/).textContent).toBe("2");
-    });
+    await user.click(screen.getByTitle(/Shopping cart/));
+    fireEvent.click(screen.getByTitle(/add one/i));
+    expect(screen.getByTitle(/Quantity/).textContent).toBe("2");
   });
 });
